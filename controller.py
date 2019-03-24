04296e498ff952944cfb51261cecd715dfad4752
cedric-d <cedric-d@users.noreply.github.com>
2019-03-24 16:17:50 +0100

    WIP

--- controller.py
+++ controller.py
@@ -2,7 +2,9 @@
 
 import argparse
 import asyncio
+import functools
 import logging
+import signal
 
 import aiohttp
 from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
@@ -12,10 +14,11 @@ import socketio
 
 
 class SignalingSession(socketio.AsyncClientNamespace):
-    def __init__(self, url):
+    def __init__(self, url, controller):
         socketio.AsyncClientNamespace.__init__(self)
         self._sio = None
         self._url = url
+        self._controller = controller
 
     async def create(self):
         self._sio = socketio.AsyncClient()
@@ -23,23 +26,23 @@ class SignalingSession(socketio.AsyncClientNamespace):
         await self._sio.connect(self._url)
     
     async def attach(self, room):
-        await self._sio.emit('create or join', room)
+        await self._sio.emit('join as robot', room)
     
+    async def sendMessage(self, message):
+        await self._sio.send(message)
     
-    async def on_created(self, data):
-        print("on_created")
-    
-    async def on_full(self, data):
-        print("on_full")
-    
-    async def on_join(self, data):
-        print("on_join")
     
     async def on_joined(self, room, socketId):
-        print("Joined room {} using socket id {}".format(room, socketId))
+        print("Joined room {} with id {}".format(room, socketId))
     
     async def on_log(self, data):
         print(' '.join(data))
+    
+    async def on_ready(self, room):
+        print("Room {} is ready".format(room));
+    
+    async def on_not_ready(self, room):
+        print("Room {} is NOT ready".format(room));
     
     
     async def on_connect(self):
@@ -52,22 +55,37 @@ class SignalingSession(socketio.AsyncClientNamespace):
         print("message received with ", data)
     
 
-async def run(pc, player, session):
-    await session.create()
+class RobotController:
+    def __init__(self, url):
+        # create signaling and peer connection
+        self.session = SignalingSession(url, self)
+        self.pc = RTCPeerConnection()
+        
+        # create media source
+        self.player = MediaPlayer('/dev/video0', format='v4l2', options={
+            'video_size': '640x480'
+        })
+    
+    async def destroy(self):
+        await self.pc.close()
+        await self.session.destroy()
+    
+    async def prepare(self):
+        await self.session.create()
 
-    # configure media
-    media = {'audio': False, 'video': True}
-    if player and player.audio:
-        pc.addTrack(player.audio)
-        media['audio'] = True
+        # configure media
+        media = {'audio': False, 'video': True}
+        if self.player and self.player.audio:
+            self.pc.addTrack(self.player.audio)
+            media['audio'] = True
 
-    if player and player.video:
-        pc.addTrack(player.video)
-    else:
-        pc.addTrack(VideoStreamTrack())
+        if self.player and self.player.video:
+            self.pc.addTrack(self.player.video)
+        else:
+            self.pc.addTrack(VideoStreamTrack())
 
-    # join video room
-    plugin = await session.attach('Robot')
+        # join video room
+        plugin = await self.session.attach('robot1')
 #    await plugin.send({
 #        'body': {
 #            'display': 'aiortc',
@@ -77,29 +95,32 @@ async def run(pc, player, session):
 #        }
 #    })
 
-    # send offer
-    await pc.setLocalDescription(await pc.createOffer())
-    request = {'request': 'configure'}
-    request.update(media)
-    response = await plugin.send({
-        'body': request,
-        'jsep': {
-            'sdp': pc.localDescription.sdp,
-            'trickle': False,
-            'type': pc.localDescription.type
-        }
-    })
+#    # send offer
+#    await pc.setLocalDescription(await pc.createOffer())
+#    request = {'request': 'configure'}
+#    request.update(media)
+#    response = await plugin.send({
+#        'body': request,
+#        'jsep': {
+#            'sdp': pc.localDescription.sdp,
+#            'trickle': False,
+#            'type': pc.localDescription.type
+#        }
+#    })
 
-    # apply answer
-    answer = RTCSessionDescription(
-        sdp=response['jsep']['sdp'],
-        type=response['jsep']['type'])
-    await pc.setRemoteDescription(answer)
+#    # apply answer
+#    answer = RTCSessionDescription(
+#        sdp=response['jsep']['sdp'],
+#        type=response['jsep']['type'])
+#    await pc.setRemoteDescription(answer)
 
-    # exchange media for 10 minutes
-    print('Exchanging media')
-    await asyncio.sleep(600)
+#    # exchange media for 10 minutes
+#    print('Exchanging media')
+#    await asyncio.sleep(600)
 
+def sighandler(signame):
+    print("Received signal %s" % signame)
+    loop.stop()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Glob'ibulle controller")
@@ -109,24 +130,20 @@ if __name__ == '__main__':
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
-
-    # create signaling and peer connection
-    session = SignalingSession(args.url)
-    pc = RTCPeerConnection()
-
-    # create media source
-    player = MediaPlayer('/dev/video0', format='v4l2', options={
-        'video_size': '640x480'
-    })
-
+    
+    # create the controller
+    controller = RobotController(args.url)
+    
+    # prepare the event loop
     loop = asyncio.get_event_loop()
+    
+    # register signal handler
+    for signame in ('SIGINT', 'SIGTERM'):
+        loop.add_signal_handler(getattr(signal, signame), functools.partial(sighandler, signame))
+    
     try:
-        loop.run_until_complete(run(
-            pc=pc,
-            player=player,
-            session=session))
-    except KeyboardInterrupt:
-        pass
+        loop.run_until_complete(controller.prepare())
+        loop.run_forever()
     finally:
-        loop.run_until_complete(pc.close())
-        loop.run_until_complete(session.destroy())
+        loop.run_until_complete(controller.destroy())
+        loop.close()
